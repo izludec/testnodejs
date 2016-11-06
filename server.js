b092b084effd61372d2d1df3f6117edb4afa9c04
Kirill Izludec <izludec@hotmail.com>
2016-11-06 12:16:19 +0000

    second commit

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
-var express = require('express')
-var app = express()
+var http = require('http')
+var url = require('url')
+var moment= require("moment")
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+    function parsetime (time) {
+      return  ""+moment(time).format("MMMM")+" "+(moment(time).format("D"))+", "
+      +moment(time).format("YYYY")
+      
+    }
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+    function unixtime (time) {
+      return time.getTime()
+    }
+
+    var server = http.createServer(function (req, res) {
+      var parsedUrl = url.parse(req.url, true)
+      var time
+      var reg = new RegExp("%20", 'g')
+      if(parsedUrl.pathname.replace("/","")[0].match(/\D/)){
+        time = new Date(parsedUrl.pathname.replace("/","").replace(reg," "))
+      }
+      else{
+      time = new Date(parsedUrl.pathname.replace("/","")*1000)
+      }
+      var result_time
+      var result_unix
+
+     
+        result_time = parsetime(time)
+        result_unix = unixtime(time)
+        console.log(result_time)
+
+      if (result_time && result_unix) {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ "unix": result_unix/1000, "natural": result_time }))
+      } else {
+        res.writeHead(404)
+        res.end()
+      }
+    })
+    server.listen(8080,function(){
+      console.log("Started 8080")
+    })
